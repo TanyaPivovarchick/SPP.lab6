2e670ad026ac81c1246fb604e90211ca170046ca
tatsiana <tatsiana.pivovarchick@gmail.com>
2017-11-23 03:19:49 +0300

    add thread pool and my task

---
+++ ThreadPool/Logic/MyTask.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logic
+{
+    class MyTask
+    {
+        public delegate void CallbackDelegate(object state);
+
+        public CallbackDelegate callback;
+        public object state;
+
+        public MyTask(CallbackDelegate callback, object state)
+        {
+            this.callback = callback;
+            this.state = state;
+        }
+    }
+}
---
+++ ThreadPool/Logic/MyThreadPool.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Logic
+{
+    public class MyThreadPool : IMyThreadPool
+    {
+        private const int MAX_COUNT_OF_TASKS = 2;
+        private const int WAITING_TIME = 1000;
+
+        private int MinCountOfThreads { get; set; }
+        private int MaxCountOfThreads { get; set; }
+
+        private object dictionarySync = new object();
+        private object queueSync = new object();
+
+        private Queue<MyTask> queueOfTasks = new Queue<MyTask>();
+        private List<int> listOfThreads = new List<int>();
+
+        private CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+
+        private static AutoResetEvent newTaskEvent = new AutoResetEvent(true);
+
+        public MyThreadPool(int min, int max)
+        {
+            SetThreadPoolСharacteristics(min, max);
+
+            for (int i = 0; i < MinCountOfThreads; i++)
+            {
+                AddNewThreadsInDictionary();
+            }
+        }
+
+        public void SetThreadPoolСharacteristics(int min, int max)
+        {
+            MinCountOfThreads = min;
+            MaxCountOfThreads = max;
+        }
+
+        public void AddTaskInQueue(MyTask task)
+        {
+            lock (queueSync)
+            {
+                queueOfTasks.Enqueue(task);
+                newTaskEvent.Set();
+            }
+        }
+
+        private void AddNewThreadsInDictionary()
+        {
+            var newThread = new Task(WorkForThread, cancelTokenSource.Token, TaskCreationOptions.LongRunning);
+
+            lock (dictionarySync)
+            {
+                listOfThreads.Add(newThread.Id);
+            }
+
+            newThread.Start();
+        }
+
+        public void WorkForThread()
+        {
+            MyTask currentTask;
+            bool isEvent = false;
+
+            do
+            {
+                if (queueOfTasks.Count() >= MAX_COUNT_OF_TASKS && GetCountOfThreads() < MaxCountOfThreads)
+                {
+                    AddNewThreadsInDictionary();
+                }
+
+                currentTask = TakeTaskFromQueue();
+
+                if (currentTask != null)
+                {
+                    ProcessingTask(currentTask);
+                }
+                else
+                {
+                    if (GetCountOfThreads() > MinCountOfThreads)
+                    {
+                        isEvent = newTaskEvent.WaitOne(WAITING_TIME);
+                    }
+                    else
+                    {
+                        isEvent = newTaskEvent.WaitOne();
+                    }
+
+                    if (!isEvent)
+                    {
+                        if (GetCountOfThreads() > MinCountOfThreads)
+                        {
+                            lock (dictionarySync)
+                            {
+                                listOfThreads.Remove((int)Task.CurrentId);
+                                cancelTokenSource.Cancel();
+                            }
+                        }
+                    }
+                }
+            }
+            while (true);
+        }
+
+        private int GetCountOfThreads()
+        {
+            lock (dictionarySync)
+            {
+                return listOfThreads.Count();
+            }
+        }
+
+        private MyTask TakeTaskFromQueue()
+        {
+            lock (queueSync)
+            {
+                if (queueOfTasks.Count != 0)
+                {
+                    return queueOfTasks.Dequeue();
+                }
+                else return null;
+            }
+        }
+
+        private void ProcessingTask(MyTask task)
+        {
+            task.callback(task.state);
+        }
+    }
+}
